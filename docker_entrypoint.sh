87bf8ee0d533eb41a14d3555d78b8d4a6a03a4fb
Evan Purkhiser <evanpurkhiser@gmail.com>
2023-01-12 02:01:50 -0800

    Inherit SENTRY_AUTH_TOKEN env

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-sentry-cli --api-key "$SENTRY_API_KEY" monitors run "$SENTRY_MONITOR_ID" -- pdm run venmo-auto-cashout
+sentry-cli monitors run "$SENTRY_MONITOR_ID" -- pdm run venmo-auto-cashout
