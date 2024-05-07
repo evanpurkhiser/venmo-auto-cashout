16e8ebd39c09bdb2f740794f313d7982b07fb59d
Michael Bianco <iloveitaly@gmail.com>
2024-05-07 17:00:20 -0600

    remove unneeded comment

--- dockerStart.sh
+++ dockerStart.sh
@@ -12,7 +12,5 @@ else
 	# stdout redirect ensures logs are redirected to the parent process https://snippets.aktagon.com/snippets/945-how-to-get-cron-to-log-to-stdout-under-docker-and-kubernetes
 	echo "${SCHEDULE} root sh -lc 'cd ${PWD} && $(which pdm) run venmo-auto-cashout' > /proc/1/fd/1 2>&1" >>/etc/crontab
 
-	# debian slim does not include cron by default, this must be installed via Dockerfile
-	# additionally, busybox does *not* add the cron process, so we must use the debian cron package
 	cron -L 8 -f
 fi
